3a7cac0b968ceb6258f464a62cdd82a41ee87e94
henrry <henrrywelson@outlook.com>
2020-11-03 14:57:11 +0800

    Day 87

---
+++ Vue与React高级框架开发/项目实战/项目：Vue仿去哪儿网/qunar/src/pages/home/components/Icons.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="home-icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img :src="item.iconUrl" class="icon-img-content" />
+          </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: 1,
+          iconUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/158387fe5376294f3776d01358d6b73b.png',
+          desc: '景点门票'
+        },
+        {
+          id: 2,
+          iconUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/50357a9f88fe84bcf9d20961059c8611.png',
+          desc: '上海迪士尼'
+        },
+        {
+          id: 3,
+          iconUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+          desc: '东方明珠'
+        },
+        {
+          id: 4,
+          iconUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/aba25fb84648c130561afa5398d3552a.png',
+          desc: '野生动物园'
+        },
+        {
+          id: 5,
+          iconUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+          desc: '豫园'
+        },
+        {
+          id: 6,
+          iconUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+          desc: '上海水族馆'
+        },
+        {
+          id: 7,
+          iconUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/f9b2f683354d575710af0938cfef675e.png',
+          desc: '杜莎蜡像馆'
+        },
+        {
+          id: 8,
+          iconUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/69e8b07cb2d438c5530aebd4c8e3abd3.png',
+          desc: '一日游'
+        },
+        {
+          id: 9,
+          iconUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/89545f7aa8451b0979f7fcfd5536b7c1.png',
+          desc: '上海海昌'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+.home-icons >>> .swiper-container
+  height 0
+  padding-bottom 50%
+.home-icons
+  margin-top 0.1rem
+  .icon
+    position relative
+    overflow hidden
+    float left
+    width 25%
+    height 0
+    padding-bottom 25%
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom 0.44rem
+      box-sizing border-box
+      padding 0.1rem
+      .icon-img-content
+        display block
+        margin 0 auto
+        height 100%
+    .icon-desc
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      height 0.44rem
+      line-height 0.44rem
+      text-align center
+      color $darkTextColor
+      ellipsis()
+</style>
--- Vue与React高级框架开发/项目实战/项目：Vue仿去哪儿网/qunar/src/pages/home/components/Swiper.vue
+++ Vue与React高级框架开发/项目实战/项目：Vue仿去哪儿网/qunar/src/pages/home/components/Swiper.vue
@@ -2,13 +2,9 @@
   <div class="home-swiper">
     <swiper :options="swiperOption">
       <!-- slides -->
-      <swiper-slide>I'm Slide 1</swiper-slide>
-      <swiper-slide>I'm Slide 2</swiper-slide>
-      <swiper-slide>I'm Slide 3</swiper-slide>
-      <swiper-slide>I'm Slide 4</swiper-slide>
-      <swiper-slide>I'm Slide 5</swiper-slide>
-      <swiper-slide>I'm Slide 6</swiper-slide>
-      <swiper-slide>I'm Slide 7</swiper-slide>
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img :src="item.imgUrl" class="swiper-img" />
+      </swiper-slide>
       <!-- Optional controls -->
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
@@ -19,9 +15,33 @@ export default {
   name: 'HomeSwiper',
   data () {
     return {
-      swiperOption: {}
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '1',
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg'
+        },
+        {
+          id: '2',
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1811/b8/5d599bbdcf8b57.jpg_r_640x214_2ee055e3.jpg'
+        }
+      ]
     }
   }
 }
 </script>
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.home-swiper>>>.swiper-pagination-bullet-active
+  background #fff
+.home-swiper
+  width 100%
+  height 0
+  padding-bottom 35.25%
+  overflow hidden
+  background #eee
+  .swiper-img
+    width 100%
+</style>
