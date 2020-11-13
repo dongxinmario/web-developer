a65c8c6802baa0c3ded4fa35b5c36e45ba6944cf
henrry <henrrywelson@outlook.com>
2020-11-13 09:18:17 +0800

    Day 93

--- Vue与React高级框架开发/项目实战/项目：Vue仿去哪儿网/qunar/src/main.js
+++ Vue与React高级框架开发/项目实战/项目：Vue仿去哪儿网/qunar/src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -10,12 +8,9 @@ import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 Vue.use(VueAwesomeSwiper)
-Vue.config.productionTip = false
-/* eslint-disable no-new */
+Vue.config.productionTip = false 
 new Vue({
-  el: '#app',
   router,
   store,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
---
+++ Vue与React高级框架开发/项目实战/项目：Vue仿去哪儿网/qunar/vue.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('styles', path.join(__dirname, './src/assets/styles'))
+      .set('@', path.join(__dirname, './src/'))
+      .set('common', path.join(__dirname, './src/components/'))
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        pathRewrite: {
+          '^/api': '/mock'
+        }
+      }
+    }
+  }
+}
